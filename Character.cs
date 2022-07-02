d2d7116fc1eaf149ec66f8dd27bc1fe8907badd7
Wars-bars <108578181+Wars-bars@users.noreply.github.com>
2022-07-02 16:23:14 +0300

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int health;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
