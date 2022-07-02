df49468efa997f50f15918a6e72c0d4e3315fe75
NikitaBukreyev <73642125+NikitaBukreyev@users.noreply.github.com>
2022-07-02 16:12:08 +0300

    Add files via upload

---
+++ c#/far.cs
@@ -0,0 +1,14 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp 
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
